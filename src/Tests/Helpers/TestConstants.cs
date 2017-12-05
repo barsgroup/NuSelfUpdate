c564227ace4d01f3c46516f505a4fa67201a0504
Виктор Борисов <viktor.borisov@bars-open.ru>
2017-12-05 17:15:07 +0700

    Добавилен метод DropAppFolder

--- src/Tests/Helpers/TestConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NuSelfUpdate.Tests.Helpers
-{
-    public static class TestConstants
-    {
-        public const string AppDirectory = @"c:\app\";
-        public const string AppPackageId = "package";
-    }
-}
